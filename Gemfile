5fc0f7821cb7301c1668108f3f91b788eefebec7
Asri Nurul Ma'rifah <asrimarifah0402@gmail.com>
2021-12-06 21:46:06 +0700

    new

--- Gemfile
+++ Gemfile
@@ -48,6 +48,8 @@ group :development do
   # Display performance information such as SQL time and flame graphs for each request in your browser.
   # Can be configured to work on production as well see: https://github.com/MiniProfiler/rack-mini-profiler/blob/master/README.md
   gem 'rack-mini-profiler', '~> 2.0'
+  gem 'guard', '~> 2.18'
+  gem 'guard-livereload', '~> 2.5', require: false
 end
 
 group :test do
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_06_143426) do
+ActiveRecord::Schema.define(version: 2021_12_06_144422) do
+
+  create_table "discussions", charset: "utf8mb4", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
