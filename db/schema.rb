fdb234df547925f0a9fa7f4973f842d863ace6be
Asri Nurul Ma'rifah <asrimarifah0402@gmail.com>
2021-12-07 22:24:26 +0700

    roles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_07_144756) do
+ActiveRecord::Schema.define(version: 2021_12_07_150602) do
 
   create_table "channels", charset: "utf8mb4", force: :cascade do |t|
     t.string "channel"
